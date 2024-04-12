9b3b1ef60c7ea78e9ea4fb48a7347671ba062297
Athenais83 <athenaisrudolf@gmail.com>
2024-04-12 10:47:37 +0200

    fichier app.js

--- front/pages/App.js
+++ front/pages/App.js
@@ -1,1 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header';
+import Banner from '../components/Banner';
+import Footer from '../components/Footer';
+import Form from '../components/Form';
+import '../pages/AppRouter';
+import '../pages/Home';
+
+function App(){
+    return(
+        <>
+        <div>
+            <Header/>
+        </div>
+        <div>
+            <Banner/>
+        </div>
+        <div>
+            <Footer/>
+        </div>
+        </>
+    )
+}
+
+export default App;
