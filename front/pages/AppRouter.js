afeab6c932469f214710f1ad2ea58a6892eceff7
Athenais83 <athenaisrudolf@gmail.com>
2024-04-12 10:31:11 +0200

    creation pages

---
+++ front/pages/AppRouter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Index from '../pages/Index'
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+
+const AppRouter = (){
+    return(
+        <div>
+        <Router>
+        <Routes>
+        <Route path="/"</Routes></Router></div>
+    )
+}
