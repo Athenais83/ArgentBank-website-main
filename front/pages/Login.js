dc88aabd797936155703ec8ec08c6b184bd5fe18
Athenais83 <athenaisrudolf@gmail.com>
2024-04-12 10:38:52 +0200

    page login

--- front/pages/Login.js
+++ front/pages/Login.js
@@ -1,1 +1,16 @@
 import React from 'react'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Form from '../components/Form';
+
+function Login (){
+    return(
+        <body>
+            <Header />
+            <Form />
+            <Footer />
+        </body>
+    )
+}
+
+export default Login;
