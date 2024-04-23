ba0f5a1a082f9a8849e92a3f617d72e39b2e85ea
Athenais83 <athenaisrudolf@gmail.com>
2024-04-23 14:55:43 +0200

    logo argent bank

--- front/src/components/Menu.jsx
+++ front/src/components/Menu.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import "../App";
 import { Link, useLocation } from 'react-router-dom';
 import '../sass/_header.scss'
+import argentBankLogo from "../img/argentBankLogo.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faCircleUser} from '@fortawesome/free-solid-svg-icons';
+
 
 function Menu (){
   const location = useLocation();
@@ -11,12 +15,13 @@ function Menu (){
         <Link to="/" className={location.pathname=== '/' ? 'main-nav-logo' : 'link active'}>Home</Link>
           <img
             class="main-nav-logo-image"
-            src="../img/argentBankLogo.png"
+            src={argentBankLogo}
             alt="Argent Bank Logo"
           />
           <h1 class="sr-only">Argent Bank</h1>
           <Link to="/Login" className={location.pathname==='/Login' ? 'main-nav-item' : 'link active'}>Sign in</Link>
             <i class="fa fa-user-circle"></i>
+            <FontAwesomeIcon icon={faCircleUser} />
       </nav>
       </>
       
