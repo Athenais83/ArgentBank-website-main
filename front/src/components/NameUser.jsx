d93caaaa858421287f261db866717b527dfc3b92
Athenais83 <athenaisrudolf@gmail.com>
2024-04-26 12:04:55 +0200

    ...

--- front/src/components/NameUser.jsx
+++ front/src/components/NameUser.jsx
@@ -5,7 +5,7 @@ import '../sass/_nameUser.scss';
 function NameUser (){
     return(
         <div className="bg-dark">
-        <h1>Welcome back<br />Tony Jarvis!</h1>
+        <h1 className="Name">Welcome back<br />Tony Jarvis!</h1>
         <button className="edit-button">Edit Name</button>
       </div>
     )
