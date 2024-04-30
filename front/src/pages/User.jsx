76d56a790a6e70da5cfdbdbc452bd02edfc867b3
Athenais83 <athenaisrudolf@gmail.com>
2024-04-30 18:03:16 +0200

    changement swagger

--- front/src/pages/User.jsx
+++ front/src/pages/User.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from "react-router-dom";
 
-// import './user.scss'
 import Transactions from '../components/Transactions';
 import EditUser from '../components/EditUser';
 
@@ -11,7 +10,6 @@ function User() {
    const token = useSelector((state) => state.auth.token);
    const navigate = useNavigate();
 
-   // Si l'utilisateur n'est pas connecté, redirection vers la page de connexion
    useEffect(() => {
       if (!token) {
          navigate("/signin");
